e099f68bf37a279bc9ce882db140878041abeca2
LewisTao <huytaoquang@gmail.com>
2015-06-29 21:54:40 +0700

    [ADD AUTHORS] Add devise authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :authors
   namespace :api, defaults: { format: :json }, path: '/' do
     namespace :v1 do
-
+      resources :authors, only: [:index]
     end
 
   end
