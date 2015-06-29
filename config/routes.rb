94196298c0bad7d665ab0f11fa328b95e1908d7c
LewisTao <huytaoquang@gmail.com>
2015-06-29 23:45:19 +0700

    [ADD AUTHORS] Set devise for version api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :authors, :controllers => {:registrations => "api/v1/authors"}
+
   namespace :api, defaults: { format: :json }, path: '/' do
     namespace :v1 do
+      devise_for :authors, :controllers => {:registrations => "api/v1/authors"}
       resources :authors, only: [:index, :show]
     end
 
